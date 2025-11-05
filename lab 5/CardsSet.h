f393f8a6d87c23d0447d50598ceebe4ce0953852
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-11-04 20:58:04 -0500

    Exersise 1 and some of Exersise 2
    Exersise 1 completed
    In Exersise 2:
     - novSet()
     - shuffle()
     - take()
    methods are completed

---
+++ lab 5/CardsSet.h
@@ -0,0 +1,55 @@
+#include <cstdlib>
+#include <ctime>
+class CardsSet {
+public:
+    CardsSet() : number(0) {}
+    void novSet();
+    void shuffle();
+    int numCards() { return number; }
+    Card take();
+    void put(Card k);
+    Card lookIn(int no);
+    void empty() { number = 0; }
+private:
+    Card set[52];
+    int number;
+};
+
+//create set of cards
+void CardsSet::novSet(){
+    int number = 0; //counter to access index
+    for (int col = club; col<=spade;col++){ //for each color
+        for(int val = 1; val<=13;val++){ //give value
+            set[number] = Card((color)col,val); //put it in set and index cnt --> creating Card object with Color and Value arguments
+            number++; //increase counter
+        }
+    }
+
+}
+
+//shuffle method
+void CardsSet::shuffle(){
+    //sets new seed --> ensures different random number generated each time
+    srand(time(0)); 
+    for(int i=0; i<number;i++){ //iterate over the number of cards
+        int j=rand()%number; //give random index between 0 and the size of number (initally going to be 52)
+        //swap card positins
+        Card tmp = set[i];
+        set[i]=set[j];
+        set[j]=tmp;
+    }
+}
+
+//take method
+Card CardsSet::take(){
+    assert(number>0); //check if number of cards is greater than 0
+    Card top = set[0]; //get top card
+
+    //shift other elements left
+    for (int i=1; i<number;i++){
+        set[i-1]=set[i];
+    }
+    number--; //reduce the number of cards 
+    return top; //return top card
+
+}
