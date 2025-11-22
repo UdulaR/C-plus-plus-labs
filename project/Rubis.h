5a7a687757a02a6014baf260dfabc55988d0c923
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-11-21 20:33:03 -0500

    Finished Rubis.h
    Finished Rubis.h

--- project/Rubis.h
+++ project/Rubis.h
@@ -2,10 +2,25 @@
 using namespace std;
 
 class Rubis{
-    private:
-        Rubis(){
-            
+    public:
+        operator int() const{
+            return value;
         }
+
+        friend ostream& operator<<(ostream& output, const Rubis& r);
+
+
+    private:
+        int value;
+        Rubis(int val){
+            this->value=val;
+        }
+        friend class RubisDeck;
 
 
 };
+
+ostream& operator<<(ostream&output, const Rubis& r){
+    output<<r.value;
+    return output;
+}
