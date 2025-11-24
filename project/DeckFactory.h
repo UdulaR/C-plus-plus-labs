5db6e4545f82b7d902c1c4e312b95eb4f37653c0
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-11-23 20:21:57 -0500

    DeckFactory.h finished
    Finished DeckFactory

--- project/DeckFactory.h
+++ project/DeckFactory.h
@@ -1,7 +1,43 @@
+#include <random>
+#include <algorithm> //for random_shuffle
+#include <vector>
+
 using namespace std;
 
 template <class C> class DeckFactory{
-    void shuffle();
-    C* getNext();
-    bool isEmpty() const;
+    private:
+        vector<C*> deck; //deck = vector to store pointers of type C*
+
+    public:
+        //constructor
+        DeckFactory(){}
+
+        //add element to deck
+        void add(C* elem){
+            deck.push_back(elem);
+        }
+
+        //fuction to shuffle rubis or cards
+        void shuffle(){
+            random_shuffle(deck.begin(), deck.end());
+        }
+
+        //get next card/rubis
+        C* getNext(){
+            if(deck.empty()){ //if the deck is empty return a null pointer
+                return nullptr;
+            }
+            else{
+                C* next = deck.back(); //put last element in vector into next
+                deck.pop_back(); //remove last element from vector
+                return next;
+            }
+
+        }
+
+        //checks if vector is empty
+        bool isEmpty() const{
+            return deck.empty();
+        }
 };
+
